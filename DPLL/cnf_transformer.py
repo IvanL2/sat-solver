803354dc89e60c42f23cb957ade3f9a5731b526d
Ivan Ly <ivan.ly@student.manchester.ac.uk>
2022-11-16 22:23:45 +0000

    add cnf rewriter, add clause splitter

--- DPLL/cnf_transformer.py
+++ DPLL/cnf_transformer.py
@@ -7,18 +7,81 @@ class Transformer:
         pass
     
     def transform(self, tree : Tree):
-        newtree = tree
+        names = self.naming(tree)
+        clauses = set()
+        for x in names:
+            self.to_cnf(x)
+            clauses = clauses.union(self.generate_clauses(x))
+        return clauses
+        
+    def generate_clauses(self, tree : Tree) -> set:
+        temp = set()
+        if isinstance(tree, Tree):
+            if isinstance(tree.get_root(), Conjunction):
+                temp = temp.union(self.generate_clauses(tree.get_left()))
+                temp = temp.union(self.generate_clauses(tree.get_right()))
+                return temp
+        temp.add(tree)
+        return temp
 
+    def to_cnf(self, tree : Tree):
+        while True:
+            if not self._is_operator_in_tree(tree, Equivalence()):
+                if not self._is_operator_in_tree(tree, Implication()):
+                    if not self._is_conjunction_under_disjunction(tree):
+                        break
+            self._rewrite_tree(tree)
+
+            
+    def _rewrite_tree(self, tree: Tree):
+        if not isinstance(tree, Tree):
+            return None
+        if isinstance(tree.get_root(), Equivalence):
+            _Equivalence_Rules.equivalence(tree)
+        elif isinstance(tree.get_root(), Implication):
+            _Equivalence_Rules.implication(tree)
+        elif isinstance(tree.get_root(),Disjunction):
+            if self._is_conjunction_under_disjunction(tree):
+                _Equivalence_Rules.disjunction(tree)
+        elif isinstance(tree.get_root(),Negation):
+            if (isinstance(tree.get_left(), Tree)): # ¬¬a = a
+                if (isinstance(tree.get_left().get_root(), Negation)):
+                    tree = tree.get_left().get_left()
+            elif (isinstance(tree.get_left(), Tree)): # negation only allowed on literals (¬a never ¬(a v b) etc )
+                _Equivalence_Rules.negation(tree.get_root())
+        if isinstance(tree, Tree):
+            self._rewrite_tree(tree.get_left())
+            self._rewrite_tree(tree.get_right())
+
+    def _is_conjunction_under_disjunction(self, tree: Tree, disj=False) -> bool:
+        if not isinstance(tree, Tree):
+            return False
+        if isinstance(tree.get_root(), Disjunction):
+            return self._is_conjunction_under_disjunction(tree.get_left(), disj=True) or self._is_conjunction_under_disjunction(tree.get_right(), disj=True)
+        if isinstance(tree.get_root(), Conjunction):
+            if disj == True:
+                return True
+        return self._is_conjunction_under_disjunction(tree.get_left(), disj=disj) or self._is_conjunction_under_disjunction(tree.get_right(), disj=disj)
+
+            
+    def _is_operator_in_tree(self, tree: Tree, op: Operator) -> bool:
+        if not isinstance(tree, Tree): # variable/T/F reached
+            return False
+        if isinstance(tree.get_root(), Operator):
+            if tree.get_root().operator == op.operator:
+                return True
+        return self._is_operator_in_tree(tree.get_left(), op) or self._is_operator_in_tree(tree.get_right(), op)
+
+    def naming(self, tree: Tree) -> set:
+        newtree = tree
         self._add_depth(newtree,0)
         p = Polarity()
         p.polarise(newtree,pol=1)
         clauses = set()
         numberofnames = 0
-        while (self._get_highest_depth(newtree) > 1):
+        while (self._get_highest_depth(newtree) > 2):
             depth = self._get_highest_depth(newtree) -1
-            print("depth",depth)
             nodes = self._get_nodes_at_depth(newtree, depth)
-            print("nodes",nodes)
             for x in nodes:
                 temp = Tree()
                 if (x.pol == 1):
@@ -41,11 +104,10 @@ class Transformer:
                     numberofnames=numberofnames+1
                 temp.depth = depth
                 clauses.add(temp)
-                print(x)
                 self._replace_node_in_tree(newtree, x, var)
         clauses.add(newtree)
         return clauses
-        
+
     def _replace_node_in_tree(self, tree, node, newnode):
         if not hasattr(tree, "is_tree"):
             return None
@@ -119,3 +181,63 @@ class Polarity:
         elif isinstance(node.get_root(), Equivalence):
             self.polarise(node.get_left(), pol=0)
             self.polarise(node.get_right(), pol=0)
+
+class _Equivalence_Rules:
+
+    def negation(tree : Tree):
+        pass
+    def equivalence(tree : Tree):
+        a = tree.get_left()
+        b = tree.get_right()
+        t1, t2, t3, t4 = Tree(), Tree(), Tree(), Tree()
+        t1.set_root(Negation())
+        t1.set_left(a)
+        t4.set_root(Negation())
+        t4.set_left(b)
+        t2.set_root(Disjunction())
+        t2.set_left(t1)
+        t2.set_right(b)
+        t3.set_root(Disjunction())
+        t3.set_left(a)
+        t3.set_right(t4)
+        tree.set_left(t2)
+        tree.set_right(t3)
+        tree.set_root(Conjunction())
+
+    def implication(tree : Tree):
+        tree.set_root(Conjunction())
+        temp = tree.get_left()
+        temptree = Tree()
+        temptree.set_root(Negation())
+        temptree.set_left(temp)
+        tree.set_left(temptree)
+
+    def disjunction(tree : Tree):
+        if not isinstance(tree, Tree):
+            return None
+        if isinstance(tree.get_left(), Conjunction):
+            l = tree.get_left()
+            r = tree.get_right()
+        elif isinstance(tree.get_right(), Conjunction):
+            l = tree.get_right()
+            r = tree.get_left()
+        else:
+            l = tree.get_left()
+            r = tree.get_right()
+        a = l.get_left()
+        b = l.get_right()
+        c = r
+        # format: (a /\ b) \/ c   -->   (a \/ c) /\ (b \/ c)
+
+        t = Tree()
+        tl = Tree()
+        tr = Tree()
+        tree.set_left(tl)
+        tree.set_right(tr)
+        tree.set_root(Conjunction())
+        tl.set_root(Disjunction())
+        tl.set_left(a)
+        tl.set_right(c)
+        tr.set_root(Disjunction())
+        tr.set_left(b)
+        tr.set_right(c)
