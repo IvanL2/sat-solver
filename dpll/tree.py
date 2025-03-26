f67a9dfc9b1f231d6ba631ee9664a8d7f899ff09
IvanL2 <in.ly@hotmail.com>
2025-03-26 21:28:59 +0000

    Housekeeping

--- dpll/tree.py
+++ dpll/tree.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional, Any
+
+
 @dataclass(eq=False)
 class Tree:
     value: Any
@@ -11,7 +13,7 @@ class Tree:
     def __str__(tree: "Tree", first: bool = True):
         if tree is None:
             return ""
-        if str(tree.value) in ["¬","&","|",">","="]:
+        if str(tree.value) in ["¬", "&", "|", ">", "="]:
             if str(tree.value) == "¬":
                 left = "" if tree.left is None else tree.left.__str__(first=False)
                 return "¬" + left
@@ -24,32 +26,3 @@ class Tree:
 
     def __repr__(self):
         return self.__str__()
-
-    # __slots__ = ("value", "left", "right", "literal", "pol")
-
-    # """Typical tree data structure, with parent information"""
-
-    # def __init__(self, value, left: "Tree" = None, right: "Tree" = None, pol: int = None, literal: bool = False):
-    #     self.left = left
-    #     if self.left is not None:
-    #         self.left.parent = self
-    #     self.right = right
-    #     if self.right is not None:
-    #         self.right.parent = self
-    #     self.value = value
-    #     self.pol = pol
-    #     self.literal = literal
-
-    # def __deepcopy__(self: "Tree", memo) -> "Tree":
-    #     left_tree = None
-    #     right_tree = None
-    #     if (self.left is not None):
-    #         left_tree = self.left.__deepcopy__(memo)
-    #     if (self.right is not None):
-    #         right_tree = self.right.__deepcopy__(memo)
-    #     new_tree = Tree(self.value, parent=self.parent, left=left_tree, right=right_tree)
-    #     if hasattr(new_tree, "literal"):
-    #         new_tree.literal = self.literal
-    #     if hasattr(new_tree, "pol"):
-    #         new_tree.pol = self.pol
-    #     return new_tree
